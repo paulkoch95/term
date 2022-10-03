80382d62a7027b2dfb01abc0e46fa843c6c3f24a
Paul <paulkoch95@googlemail.com>
2022-10-03 21:07:55 +0200

    layout fix

--- usage.py
+++ usage.py
@@ -9,8 +9,14 @@ __status__ = "development"
 from core import App, Text
 from layouter import TemplateGridLayout, ColumnLayout
 app = App()
-l = ColumnLayout("Grund Layout", app.handle, 6, app.width)
-text =  Text(app.handle, 50, 5, "Eins")
+l = ColumnLayout("Grund Layout", app.handle, 3, app.width)
+# text =  Text(app.handle, 50, 5, "Eins")
+text = ColumnLayout("Layout 2", app.handle, 2)
+text.add_widget(Text(app.handle, 0,0,"Test 2-1"))
+c2 = ColumnLayout("Layout3", app.handle, 2)
+c2.add_widget(Text(app.handle, 0,0,"Test 2-1-2"))
+c2.add_widget(Text(app.handle, 0,0,"Test 2-1-3"))
+text.add_widget(c2)
 text2 =  Text(app.handle, 80, 5, "Zwei")
 text3 =  Text(app.handle, 45, 5, "Drei")
 l.add_widget(text)
