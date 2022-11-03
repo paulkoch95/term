57fa3b04077485056835a4831bbeed0a4cc4c732
paulkoch95 <paulkoch95@googlemail.com>
2022-11-03 14:38:39 +0100

    Layout Bugfix -fixed layouting issues with several widgets

--- usage.py
+++ usage.py
@@ -9,12 +9,17 @@ from core import App
 from widgets.text import Text, AutoScrollText
 from widgets.clock import Clock
 from widgets.shapes import FilledBlock
-from widgets.plot import BarPlot
+from widgets.plot import BarPlot, ScatterGrid
 from widgets.layouter import TemplateGridLayout, ColumnLayout
 app = App()
 
-base_layout = ColumnLayout("Base Layout", app.handle, 0,0,2, app.width,10,highlight_border=True)
+base_layout = ColumnLayout("Base Layout", app.handle, 0,0,2, app.width,30,highlight_border=True)
 base_layout.add_widget(BarPlot(app.handle, 0, 0, 10, 10))
-base_layout.add_widget(FilledBlock(app.handle, 0, 0, 5, 5 ))
+# base_layout.add_widget(FilledBlock(app.handle, 10, 10, 5, 5 ))
+
+sg = ScatterGrid(app.handle, 10, 10, 5, 5 )
+sg.plot([["1", "2", "3"], ["4", " ", "6"], ["", "8", "9"]])
+base_layout.add_widget(sg)
+
 app.add_widget(base_layout)
 app.render()
--- widgets/plot.py
+++ widgets/plot.py
@@ -20,8 +20,24 @@ class BarPlot(Renderable):
             self.bar_height += 1
 
     def render(self) -> None:
-        Drawing.h_line_staggered(self._ctx, self._y + 27, self._x, 25, 5)
-        Drawing.v_line_staggered(self._ctx, self._y + 25, self._x, 25, 5)
-        Drawing.block_v_line(self._ctx, self._y + 24, self._x + 1, self.bar_height)
-        Drawing.block_v_line(self._ctx, self._y + 24, self._x + 2, self.bar_height)
-        Drawing.block_v_line(self._ctx, self._y + 24, self._x + 4, self.bar_height)
+        Drawing.h_line_staggered(self._ctx, self._y + self.height - 3, self._x, self.width, 5)
+        # Drawing.v_line_staggered(self._ctx, self._y + (self.height+3), self._x, self.height, 5)
+
+        # Drawing.block_v_line(self._ctx, self._y + 24, self._x + 1, self.bar_height)
+        # Drawing.block_v_line(self._ctx, self._y + 24, self._x + 2, self.bar_height)
+        # Drawing.block_v_line(self._ctx, self._y + 24, self._x + 4, self.bar_height)
+
+
+class ScatterGrid(Renderable):
+    def __init__(self, render: curses.window, x: int, y: int, w: int, h: int):
+        super().__init__(render, x, y)
+        self.data: list[list] = [[]]  # [ - rows [ - cols]]
+        # self.data = [["1", "2", "3"], ["4", " ", "6"], ["", "8", "9"]]
+
+    def plot(self, data: list[list]) -> None:
+        self.data = data
+
+    def render(self):
+        for x, row in enumerate(self.data):
+            for y, col in enumerate(row):
+                Drawing.draw_text_label(self._ctx, self._y + y, self._x + x, col)
