31f0742e472f80d702ec6884a2f7e0549e2fe452
paulkoch95 <paulkoch95@googlemail.com>
2022-12-04 19:39:56 +0100

    Documentation Fix - housekeeping

--- widgets/plot.py
+++ widgets/plot.py
@@ -20,21 +20,44 @@ class BarSlot:
 
 
 class BarPlot(Renderable):
+    """
+    Bar Plot Widget:
+    Renders a horizontal Bar Plot
 
+    Vertical Axis (y - Axis) (fills entire height available to widget)
+    |
+    v
+    |   # <- Single Column Data Point (every Data Point is BarSlot object)
+    |   #
+    |   #
+    |-----------    <- x-Axis (Horizontal Axis) (fills entire width available to widget)
+
+    Data can be added, removed or update on runtime. Axis are fully dynamic based on the current that is
+    to be plotted.
+    """
     def __init__(self, render: curses.window, x: int, y: int, w: int, h: int):
         super().__init__(render, x, y, w, h)
-        self.bar_height: int = 1
         self.bars: list[BarSlot] = []
         self.max_height: int = 0
+        self.horizontal_offset = 3
 
     def add_bar(self, val):
+        """
+        Adds a BarSlot object to the BarPlot Widget. Essentially plotting a new column of data.
+        """
         s = BarSlot(len(self.bars), f'Test Bar {len(self.bars)}', val)
         self.bars.append(s)
 
     def remove_bar(self, idx):
+        """
+        Remove a data point that is being plotted. Deletes the whole BarSlot Object from the list which also updates the drawing
+        """
         del self.bars[idx]
 
     def update_bar(self, idx, val):
+        """
+        Update a specific column BarSlot data point.
+        """
         self.bars[idx].data = val
 
     def update(self):
@@ -42,13 +65,16 @@ class BarPlot(Renderable):
             bar.data = random.randint(0,25)
 
     def render(self) -> None:
-        horizontal_offset = 3
+        # intermediate constants to allow the rendering
         self.max_height = max(self.max_height, max(bar.data for bar in self.bars))
-        data_points = Drawing.h_line_staggered(self._ctx, self._y + self.height+1, self._x+horizontal_offset, self.width, len(self.bars))
+        # horizontal x axis object which also returns the indices where a BarSlot shoudl be rendered.
+        # will be replaced by its own AXIS Class in the future to allow interchangable axis between different types of graphs
+        # and to also accept pyplot axis objects.
+        data_points = Drawing.h_line_staggered(self._ctx, self._y + self.height+1, self._x+self.horizontal_offset, self.width, len(self.bars))
         Drawing.v_line_staggered(self._ctx, self._y + self.height, self._x, self.max_height, 2)
         bar: BarSlot
         for idx, bar in enumerate(self.bars):
-            Drawing.block_v_line(self._ctx, self._y + self._h, self._x + data_points[idx]+horizontal_offset, bar.data)
+            Drawing.block_v_line(self._ctx, self._y + self._h, self._x + data_points[idx]+self.horizontal_offset, bar.data)
         # Drawing.draw_text_label(self._ctx, 0,0,str(data_points))
         # Drawing.block_v_line(self._ctx, self._y + 24, self._x + 1, self.bar_height)
         # Drawing.block_v_line(self._ctx, self._y + 24, self._x + 2, self.bar_height)
